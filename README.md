8e9b945d27d007045b538a9593cc0d10b8e22df4
Augusto Quintanilha <augustoqsantos@gmail.com>
2024-12-24 16:32:56 -0300

    1 comit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Challenge Portfólio ONE
+
+Portfólio básico desenvolvido com html, sass e javascript, para o challenge do programa ONE.
+
+## Visão Geral
+
+Este portfólio é destinado para a entrega do challenge do programa ONE - Oracle Next Education
+
+## Tecnologias Utilizadas
+
+- **HTML5**: Estruturação do conteúdo da página com a linguagem de marcação padrão da web.
+- **CSS3 (SASS/SCSS)**: CSS com superpoderes, usado para estilização da aplicação, permitindo uma abordagem mais dinâmica e modular.
+- **JavaScript**: Utilizado para a criação de interatividade no site, como formulários e integração com APIs.
+- **EmailJS**: Serviço para enviar e-mails diretamente do front-end sem a necessidade de backend, utilizado para o envio de mensagens através do formulário de contato.
+
+## Licença
+
+Este projeto está licenciado sob a Licença MIT.
---
+++ javascript/functions.js
@@ -0,0 +1,33 @@
+// EmailJS
+emailjs.init('sKYtOpqTDPPSVEDH0'); // ID do EmailJS
+
+document.querySelector('#contact form').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // Parâmetros do template EmailJS
+    const templateParams = {
+        from_name: formData.get('name'),
+        to_name: 'Augusto',
+        email: formData.get('email'),
+        subject: formData.get('subject'),
+        message: formData.get('message')
+    };
+
+    emailjs
+        .send('service_4jbxa25', 'template_7dggke2', templateParams)
+        .then(function (response) {
+            alert('Mensagem enviada com sucesso! Obrigado pelo contato.');
+            form.reset();
+        })
+        .catch(function (error) {
+            alert('Ops, ocorreu um erro. Tente novamente mais tarde.');
+            console.error('Erro:', error);
+        });
+});
+
+// Inserindo ano no footer
+const currentYear = new Date().getFullYear();
+document.getElementById('currentYear').textContent = currentYear;
